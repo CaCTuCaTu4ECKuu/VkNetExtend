ec91b382edd9fd6ac771e6cbb94ac8ff3096fecc
CaCTuCaTu4ECKuu <cactucatu4eckuu@gmail.com>
2015-11-22 04:41:21 +0300

    Файлы проекта

---
+++ MainClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VkNetExtend
+{
+    using VkNet;
+
+    public class VKController
+    {
+        private VkApi _api;
+
+        public VKController(VkApi api, int applicationId)
+        {
+            _api = api;
+        }
+    }
+}
