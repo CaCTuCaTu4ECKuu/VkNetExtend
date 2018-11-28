ea9a9c870056bdb1a8f3f3f90ea441d2017ad6a6
CaCTuCaTu4ECKuu <CaCTuCaTu4ECKuu@users.noreply.github.com>
2018-11-28 10:10:43 +0300

    Add default logger that logs into console

---
+++ VkNetExtend.MessageLongPoll/Logger/ConsoleLogger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace VkNetExtend.MessageLongPoll.Logger
+{
+    public class ConsoleLogger : ILogger
+    {
+        public LogLevel LogLevel { get; set; } = LogLevel.Information;
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return logLevel != LogLevel.None && logLevel >= this.LogLevel;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            if (IsEnabled(logLevel))
+                Console.WriteLine($"{logLevel}: {eventId} - {formatter(state, exception)}");
+        }
+    }
+}
